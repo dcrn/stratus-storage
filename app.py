2c150a53897b01ad0bf8760116908ec739e91051
Declan Curran <entityingredient@gmail.com>
2015-02-03 18:51:49 +0000

    Added storage project

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+import git
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)
